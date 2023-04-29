5ca19689dcc6fd32a3fffc747ced985376e61c42
choi138 <kidjustinchoi@gmail.com>
2023-04-29 13:27:57 +0900

    feat: add color file

---
+++ src/screens/todo/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+export const TodoScreen: React.FC = () => {
+    return (
+        <ScrollView>
+            <Text>Todo Screen</Text>
+        </ScrollView>
+    )
+}
---
+++ src/styles/color.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    purple: "#babcff",
+    red: "#fd9a9a",
+    mint: "#a1fbd1",
+    skyBlue: "#a0e2fb",
+    gray: "#cecfd3",
+}
